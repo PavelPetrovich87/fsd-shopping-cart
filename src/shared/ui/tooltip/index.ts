63c9a19c53bc102694e99735789f93913b739e8a
PavelPetrovich87 <pavel.petrovich87@gmail.com>
2026-05-04 15:52:28 +0300

    revert: restore tooltip files after accidental deletion

---
+++ src/shared/ui/tooltip/index.ts
@@ -0,0 +1,11 @@
+export {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './tooltip'
+export type {
+  TooltipContentProps,
+  TooltipProps,
+  TooltipTriggerProps,
+} from './tooltip'

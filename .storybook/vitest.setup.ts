47dd0ddffb3d4869334b60a2dab7270e40118b76
PavelPetrovich87 <pavel.petrovich87@gmail.com>
2026-05-08 14:15:11 +0300

    Adjusted chromatic version

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,7 +1,8 @@
+import * as a11yAddonAnnotations from '@storybook/addon-a11y/preview'
 import { beforeAll } from 'vitest'
 import { setProjectAnnotations } from '@storybook/react'
 import * as preview from './preview'
 
-const project = setProjectAnnotations([preview])
+const project = setProjectAnnotations([a11yAddonAnnotations, preview])
 
 beforeAll(project.beforeAll)
